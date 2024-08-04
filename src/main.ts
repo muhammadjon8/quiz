8a40a74c2c2f7c38152d9a1b000dc05199462276
muhammadjon8 <umuhammadjon22@gmail.com>
2024-08-04 20:46:48 +0500

    #

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 5000;
+async function start() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Taxi va Yuk tashish xizmati')
+    .setDescription('Har qanday joydan eshigingiz oldigacha')
+    .setVersion('1.0')
+    .addTag('taxi_delivery')
+    .build();
+  app.enableCors({
+    origin: ['*'], // Replace with your allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Specify allowed headers
+    credentials: true, // Allow cookies to be sent with requests
+  });
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  app.use(cookieParser());
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+start();
